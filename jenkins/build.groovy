757b110a7c1ca180545bd3fda7b78d75b7f5c980
Ethan Clark <eclark72713@gmail.com>
2026-06-11 13:52:56 -0400

    Changed build.groovy to not use docker tool

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -3,10 +3,7 @@
 pipeline {
     agent any
     options { timestamps(); disableConcurrentBuilds() }
-    tools { 
-        maven 'maven-3.9.16' 
-        DockerTool 'docker' 
-    }
+    tools { maven 'maven-3.9.16' }
 
     environment {
         // --- APP META ---
