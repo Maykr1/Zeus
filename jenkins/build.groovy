76361653ed2dc41afb77b98506a1ec21c41189b5
Ethan Clark <eclark72713@gmail.com>
2026-06-11 16:22:19 -0400

    Remove Docker Tool again

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -3,10 +3,7 @@
 pipeline {
     agent any
     options { timestamps(); disableConcurrentBuilds() }
-    tools { 
-        maven 'maven-3.9.16'
-        dockerTool 'docker-cli'
-    }
+    tools { maven 'maven-3.9.16' }
 
     environment {
         // --- APP META ---
