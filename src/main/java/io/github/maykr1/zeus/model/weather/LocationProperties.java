345ffbfed7c3edaf366968935bc9eb4be711b2ce
Ethan Clark <eclark72713@gmail.com>
2026-06-26 12:03:02 -0400

    Added more validation for Location

--- src/main/java/io/github/maykr1/zeus/model/weather/LocationProperties.java
+++ src/main/java/io/github/maykr1/zeus/model/weather/LocationProperties.java
@@ -3,14 +3,13 @@ package io.github.maykr1.zeus.model.weather;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 @Validated
 @ConfigurationProperties(prefix = "zeus.location")
 public record LocationProperties(
-    @NotNull
-    Double latitude,
-
-    @NotNull
-    Double longitude
+    @NotNull @Min(-90) @Max(90) Double latitude,
+    @NotNull @Min(-90) @Max(90) Double longitude
 ) {}
